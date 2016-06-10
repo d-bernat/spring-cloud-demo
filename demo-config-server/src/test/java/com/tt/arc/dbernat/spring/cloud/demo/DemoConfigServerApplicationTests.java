8c6e5713d852c1558f99deb16434f316b054479b
bernat <bernat@L-HAM-014.traveltainment.int>
2016-06-10 10:16:54 +0200

    test autocofiguration

--- demo-config-server/src/test/java/com/tt/arc/dbernat/spring/cloud/demo/DemoConfigServerApplicationTests.java
+++ demo-config-server/src/test/java/com/tt/arc/dbernat/spring/cloud/demo/DemoConfigServerApplicationTests.java
@@ -7,14 +7,23 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.tt.arc.dbernat.spring.cloud.demo.conf.DemoConfigServerApplication;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.cloud.config.environment.Environment;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoConfigServerApplication.class)
 @IntegrationTest("server.port:0")
 public class DemoConfigServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Value("${server.port}")
+    private int port;
+
+    @Test
+    public void contextLoads() {
+        
+    }
 
 }
